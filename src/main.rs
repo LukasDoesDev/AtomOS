e797e42c28667d7c346b099d663edc9f740bb8a3
luukas <lukasdoesdev@gmail.com>
2022-01-23 16:54:07 +0200

    Version 0.1 * `println!` macro * `print!` macro * Newlines in output * Lists numbers 1..=5 and then panics

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+/// The main starting point of the program
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    for i in 1..=5 {
+        println!("My number is {}", i)
+    }
+
+    panic!("My panic world");
+}
